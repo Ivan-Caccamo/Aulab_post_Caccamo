e1f294862be012d41cc9bd4630e1fbd15c734ba1
Ivan-Caccamo <caccamo.ivan8@gmail.com>
2023-06-11 16:42:11 +0200

    initial configuration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,9 @@
-<x-layout></x-layout>
+<x-layout>
+
+<div class="container-fluid p-5 bg-success text-center text-white">
+    <div class="row justify-content-center">
+    <div class="col-lg-12 display-1">
+        <h1>The Ivan's Post</h1>
+</div>
+
+</x-layout>
