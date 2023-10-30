f52684d5dfb047f46641ee51ea2f013ebe454b41
Ivan-Caccamo <caccamo.ivan8@gmail.com>
2023-10-30 15:40:54 +0100

    user_story_03.1

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,7 +2,7 @@
 
     <h1>Dashboard Admin</h1>
 
-    <x-requests-table :roleRequests="$revisorRequests" role="revisore"/>
-    <x-requests-table :roleRequests="$writerRequests" role="writer"/>
+    <requests-table :roleRequests="$revisorRequests" role="revisore"/>
+    <requests-table :roleRequests="$writerRequests" role="writer"/>
 
 </x-layout>
